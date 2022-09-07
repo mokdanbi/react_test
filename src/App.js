20d197bf09c37a98d0101d7504b08bd7cf5e093f
mokdanbi <l26tempo@naver.com>
2022-09-07 14:32:02 +0900

    0907

--- src/App.js
+++ src/App.js
@@ -1,24 +1,65 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
+const DB = [
+  {
+    id: 1,
+    content: "menu01",
+    link: "/s1",
+    submenu: [
+      { content: "smenu01-1", link: "/" },
+      { content: "smenu01-2", link: "/" },
+      { content: "smenu01-3", link: "/" }
+    ]
+  },
+  {
+    id: 2,
+    content: "menu02",
+    link: "/s2",
+    submenu: [
+      { content: "smenu02-1", link: "/" },
+      { content: "smenu02-2", link: "/" },
+      { content: "smenu02-3", link: "/" }
+    ]
+  },
+  {
+    id: 3,
+    content: "menu03",
+    link: "/s3",
+    submenu: [
+      { content: "smenu03-1", link: "/" },
+      { content: "smenu03-2", link: "/" },
+      { content: "smenu03-3", link: "/" }
+    ]
+  }
+]
+
 function App() {
+  const [CB, setCB] = useState("");
+  const [TG, setTG] = useState(false);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <header>
+      <h1 className={CB} onClick={() => setCB('on')}>LOGO</h1>
+      <h2 className={`${TG ? 'on' : ''}`}>toggle class</h2>
+      <button onClick={() => setTG(!TG)}>class 토글</button>
+      <nav className='GNB'>
+        <ul>
+          {
+            DB.map((it, idx) => <li key={idx}>
+              <a href={it.link}>{it.content}</a>
+              <ul className='smenu'>
+                {
+                  it.submenu.map((smenu, idx) => <li key={idx}>
+                    <a href={smenu.link}>{smenu.content}</a>
+                  </li>)
+                }
+              </ul>
+            </li>)
+          }
+        </ul>
+      </nav>
+    </header>
   );
 }
 
